961dbab2ec9007d2a37bf996aa73eefdc006f030
Steve Mitchell <smitchell@groupon.com>
2015-10-21 15:40:22 -0500

    Initial Commit

---
+++ RottenTomatoes/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RottenTomatoes
+//
+//  Created by Steve Mitchell on 10/21/15.
+//  Copyright © 2015 Steve Mitchell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RottenTomatoes/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RottenTomatoes
+//
+//  Created by Steve Mitchell on 10/21/15.
+//  Copyright © 2015 Steve Mitchell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
